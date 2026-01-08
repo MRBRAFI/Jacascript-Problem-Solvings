0c281d4308a466ff9c056e5c1efa26be5acf7240
learning about git <devmrbrafi@gmail.com>
2026-01-08 15:59:06 +0600

    Checking if Palindrome or not successfully solved

---
+++ Checking_Palindrome.js
@@ -0,0 +1,13 @@
+let inp = "madam";
+let distort = inp.split("");
+let reverseDistortion = distort.reverse();
+let joinDistortion = reverseDistortion.join("");
+
+if (inp.toLowerCase() == joinDistortion.toLowerCase()) {
+  console.log(joinDistortion);
+  console.log(true);
+} else {
+  console.log(joinDistortion);
+
+  console.log(false);
+}
