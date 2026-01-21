cae39d2f8314e4b3621d69b587a86a2c17b8ac2b
learning about git <devmrbrafi@gmail.com>
2026-01-22 00:04:44 +0600

    Reworked on Count Vowels 2

--- Count_Vowels_2.js
+++ Count_Vowels_2.js
@@ -1,23 +1,15 @@
 let vowels = "JavaScrIpt";
-let vowelCounter = 0;
-const vowelSplitter = vowels.split("");
 
-for (const vowel of vowelSplitter) {
-  if (
-    vowel === "a" ||
-    vowel === "e" ||
-    vowel === "i" ||
-    vowel === "o" ||
-    vowel === "u" ||
-    vowel === "A" ||
-    vowel === "E" ||
-    vowel === "I" ||
-    vowel === "O" ||
-    vowel === "U"
-  ) {
-    vowelCounter += 1;
+const countVowels = (vowels) => {
+  const vowelList = "aeiouAEIOU";
+  let vowelCounter = 0;
+
+  for (const vowel of vowels) {
+    if (vowelList.includes(vowel)) {
+      vowelCounter += 1;
+    }
   }
-}
-console.log(vowelCounter);
 
-// console.log(vowelSplitter);
+  return vowelCounter;
+};
+console.log(countVowels(vowels));
