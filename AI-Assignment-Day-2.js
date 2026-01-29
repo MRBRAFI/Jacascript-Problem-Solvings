f99d6158b39af3286d77352507cb4db9b3becfe6
learning about git <devmrbrafi@gmail.com>
2026-01-29 13:00:53 +0600

    Recratored solutions to one liner

--- AI-Assignment-Day-2.js
+++ AI-Assignment-Day-2.js
@@ -3,9 +3,7 @@
 const num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 const filterEven = (num) => {
-  const filteredNum = num.filter((even) => even % 2 === 0);
-
-  return filteredNum;
+  return num.filter((even) => even % 2 === 0);
 };
 
 console.log(filterEven(num), "From Filtering Nums");
@@ -13,9 +11,7 @@ console.log(filterEven(num), "From Filtering Nums");
 // 2. Square all numbers using map
 
 const squareNum = (num) => {
-  const squared = num.map((int) => int * int);
-
-  return squared;
+  return num.map((int) => int * int);
 };
 
 console.log(squareNum(num), "From Square");
@@ -23,11 +19,9 @@ console.log(squareNum(num), "From Square");
 // 3. Sum of all numbers
 
 const sumOfNums = (num) => {
-  const sum = num.reduce((accumulator, nextEliment) => {
+  return num.reduce((accumulator, nextEliment) => {
     return accumulator + nextEliment;
   }, 0);
-
-  return sum;
 };
 
 console.log(sumOfNums(num), "From Sum By reduce()");
@@ -35,13 +29,11 @@ console.log(sumOfNums(num), "From Sum By reduce()");
 // 4 Sum of all even numbers
 
 const sumOfEven = (num) => {
-  const filteredNum = num.filter((int) => int % 2 === 0);
-
-  const sum = filteredNum.reduce((accumulator, nextElement) => {
-    return accumulator + nextElement;
-  }, 0);
-
-  return sum;
+  return num
+    .filter((int) => int % 2 === 0)
+    .reduce((accumulator, nextElement) => {
+      return accumulator + nextElement;
+    }, 0);
 };
 
 console.log(sumOfEven(num), "From Sum Of Even");
