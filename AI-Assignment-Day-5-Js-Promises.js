edd8f54c5db3c47954af62d1286d3d1afa06cb09
learning about git <devmrbrafi@gmail.com>
2026-02-04 14:49:15 +0600

    Solved first assignment of ordering, downloading and log-in

--- AI-Assignment-Day-5-Js-Promises.js
+++ AI-Assignment-Day-5-Js-Promises.js
@@ -1,9 +1,13 @@
+// 1 Ordering Food
+
 function orderFood() {
   return (myOrder = new Promise((resolve, reject) => {
-    const ranNum = Math.ceil(Math.random() * 10);
+    const ranNum = Math.floor(Math.random() * 10) < 8 ? true : false;
 
-    if (ranNum < 8) {
-      resolve("Pizza Delivered");
+    if (ranNum) {
+      setTimeout(() => {
+        resolve("Pizza Delivered");
+      }, 2000);
     } else {
       reject("Restaurant Closed");
     }
@@ -17,3 +21,41 @@ orderFood()
   .catch((error) => {
     console.log(error);
   });
+
+// 2 Downloading File
+
+function downloadFile() {
+  return (myfile = new Promise((resolve, reject) => {
+    const success = true;
+
+    if (success) {
+      setTimeout(() => {
+        resolve("Download Complete");
+      }, 3000);
+    } else {
+      reject("Download Failed");
+    }
+  }));
+}
+
+downloadFile()
+  .then((res) => console.log(res))
+  .catch((err) => console.log(err));
+
+// 3 Checking Login
+
+function checkLogin() {
+  return (checkLogin = new Promise((resolve, reject) => {
+    const checkingSim = Math.random() <= 0.5 ? true : false;
+
+    if (checkingSim) {
+      resolve("Login Successful");
+    } else {
+      reject("Login Unsuccessful");
+    }
+  }));
+}
+
+checkLogin()
+  .then((res) => console.log(res))
+  .catch((err) => console.log(err));
