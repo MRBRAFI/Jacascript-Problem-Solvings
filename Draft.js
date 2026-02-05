5ab08d81376f6302f656c7688d6b2edc7c50e87a
learning about git <devmrbrafi@gmail.com>
2026-02-05 22:51:32 +0600

    Just loitering

--- Draft.js
+++ Draft.js
@@ -1,5 +1,5 @@
 const names = ["arif", "gabru", "bhebla", "gobirul", "aminul", "Abidul"];
 
-const filteredNames = names.filter((name) => name[0].toLowerCase() === "a");
+const filteredNames = names.filter((name) => name[0].toLowerCase() === "g");
 
 console.log(filteredNames);
